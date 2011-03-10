b01c859e10305a7099c92fb1889c787c88137336
Wincent Balin <wincent.balin@gmail.com>
2011-03-10 20:46:07 +0100

    Added decoder from image numbers to gesture names

--- dev/SurveyReport/sr.pl
+++ dev/SurveyReport/sr.pl
@@ -5,6 +5,20 @@
 use warnings;
 use strict;
 
+# Decoder of gesture names
+my @gesture_name =
+(
+  "illegal",
+  "digit", "down-left", "down", "down-right", "down-up",
+  "down-up-down", "left", "left-down", "left-right", "left-right-left",
+  "left-up", "letter", "multi-pinch-horizontal", "multi-pinch-vertical", "multi-rotate-clockwise",
+  "multi-rotate-counterclockwise", "multi-spread-horizontal", "multi-spread-vertical", "right", "right-down",
+  "right-left", "right-left-right", "right-up", "rotate-clockwise", "rotate-counterclockwise",
+  "up", "up-down", "up-down-up", "up-left", "up-right",
+  "illegal"
+);
+
+
 # If no arguments specified, die with message
 if(scalar(@ARGV) == 0)
 {
