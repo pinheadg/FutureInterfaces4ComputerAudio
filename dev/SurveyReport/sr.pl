f0f12ec1e51157de3a97bcd4dd865fa8b54fba0b
Wincent Balin <wincent.balin@gmail.com>
2011-03-10 18:34:16 +0100

    Wrote basic error handling

--- dev/SurveyReport/sr.pl
+++ dev/SurveyReport/sr.pl
@@ -1,5 +1,13 @@
-#!/usr/bin/perl -w
+#!/usr/bin/perl
 #
 # Survey report script
 
+use warnings;
+use strict;
+
+# If no arguments specified, die with message
+if(scalar(@ARGV) == 0)
+{
+  die "Usage: sr.pl file1 file2 ... filen\n";
+}
 
