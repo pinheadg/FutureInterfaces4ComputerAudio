1a9bea67edfad2440796e9f107cd6206a46d63ad
Wincent Balin <wincent.balin@gmail.com>
2011-04-07 02:23:56 +0200

    Added skeleton for the Uncharted Territories software

---
+++ dev/UnchartedTerritories/ut.cpp
@@ -0,0 +1,17 @@
+/**
+ * Main source file for Uncharted Territories software.
+ *
+ * @author Wincent Balin
+ */
+
+#include "ut.hpp"
+
+#include <cstdlib>
+
+int main(int argc, char** argv)
+{
+
+
+  return EXIT_SUCCESS;
+}
+
---
+++ dev/UnchartedTerritories/ut.hpp
@@ -0,0 +1,11 @@
+/**
+ * Main include file for Uncharted Territories software.
+ *
+ * @author Wincent Balin
+ */
+
+#ifndef UT_HPP
+#define UT_HPP
+
+#endif /* UT_HPP */
+
